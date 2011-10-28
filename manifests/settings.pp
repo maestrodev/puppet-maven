2a2f38cfdff9da6f63679240419c4764d16808b9
Carlos Sanchez <csanchez@maestrodev.com>
2011-10-28 12:04:59 +0200

    Add a maven::settings class to manage settings.xml

---
+++ manifests/settings.pp
@@ -0,0 +1,30 @@
+# Copyright 2011 MaestroDev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Class: maven::settings
+# 
+# A puppet recipe to set the contents of the settings.xml file
+#
+class maven::settings( $servers ) {
+
+  file { "/root/.m2":
+    ensure => directory,
+  } ->
+  file { "/root/.m2/settings.xml":
+    owner => "root",
+    mode  => "0600",
+    content => template("maven/settings.xml.erb"),
+  }
+
+}

60db3436507d392e602ea20740a46ee9b8b3cb00
Jean-François Roche <jfroche@affinitic.be>
2012-02-06 13:12:39 +0100

    mv install-gem to comply with auto include spec

---
+++ manifests/install-gem.pp
@@ -0,0 +1,9 @@
+define maven::install-gem ($version = '') {
+    exec { "gem $name $version":
+      path        => '/usr/bin:/opt/ruby/bin',
+      environment => "JAVA_HOME=$maven::java_home",
+      command     => "gem install $name --version $version --no-rdoc --no-ri",
+      unless      => "gem query -i --name-matches $name --version $version",
+      logoutput   => true,
+    }
+}

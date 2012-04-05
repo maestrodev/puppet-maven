78eddc4ca408ecde6d0ad4542d97b1cecda7bfc3
Brett Porter <brett@maestrodev.com>
2012-04-05 14:02:58 +1000

    add ability to specify the default repository configuration (overriding central) in the settings template

--- manifests/settings.pp
+++ manifests/settings.pp
@@ -17,7 +17,7 @@
 # A puppet recipe to set the contents of the settings.xml file
 #
 define maven::settings( $home = '/root', $user = 'root',
-  $servers = [], $mirrors = [] ) {
+  $servers = [], $mirrors = [], $default_repo_config = {} ) {
 
   file { "${home}/.m2":
     ensure => directory,
--- spec/defines/settings_spec.rb
+++ spec/defines/settings_spec.rb
@@ -1,8 +1,9 @@
 require "spec_helper"
 
+URL = 'http://localhost:8082/archiva/repository/all/'
 MIRROR = {
     'id' => 'maestro-mirror',
-    'url' => 'https://localhost:8082/archiva/repository/all/',
+    'url' => URL,
     'mirrorof' => 'external:*',
 }
 MIRROR_SERVER = {
@@ -14,6 +15,16 @@ DEPLOY_SERVER = {
     'id' => 'maestro-deploy',
     'username' => 'deploy_user',
     'password' => 'deploy_pass',
+}
+DEFAULT_REPO_CONFIG = {
+    'url' => URL,
+    'snapshots' => {
+        'enabled' => 'true',
+        'checksumPolicy' => 'fail',
+    },
+    'releases' => {
+        'checksumPolicy' => 'fail',
+    }
 }
 
 describe "maven::settings" do
@@ -45,9 +56,28 @@ describe "maven::settings" do
   expected_filename = '/home/u/.m2/settings.xml'
   it { should contain_file(expected_filename).with_owner('u') }
 
-  it 'should generate valid settings.xml when mirrors and servers are nil' do
+  it 'should generate valid settings.xml when mirrors and servers are specified' do
     content = catalogue.resource('file', expected_filename).send(:parameters)[:content]
     content.should == read_settings_file("mirror-servers-settings.xml")
+  end
+
+end
+
+describe "maven::settings" do
+  let(:title) { 'settings' }
+  let(:params) {
+    {
+        :user => "u",
+        :home => "/home/u",
+        :default_repo_config => DEFAULT_REPO_CONFIG,
+    } }
+
+  expected_filename = '/home/u/.m2/settings.xml'
+  it { should contain_file(expected_filename).with_owner('u') }
+
+  it 'should generate valid settings.xml when default repository configuration is specified' do
+    content = catalogue.resource('file', expected_filename).send(:parameters)[:content]
+    content.should == read_settings_file("default-repo-settings.xml")
   end
 
 end

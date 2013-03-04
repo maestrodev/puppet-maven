c1e84890b912ea55d907ee49f4d2cc227122b597
SHaselbauer <sarah.haselbauer@akquinet.de>
2013-03-04 11:32:41 +0100

    changed settings.pp in a way that the settings.xml can be placed anywhere, default is still [USER_HOME]/.m2/settings.xml if target_directory and target_filename are not specified differently

--- manifests/settings.pp
+++ manifests/settings.pp
@@ -16,28 +16,34 @@
 #
 # A puppet recipe to set the contents of the settings.xml file
 #
-define maven::settings( $home = undef, $user = 'root',
-  $servers = [], $mirrors = [], $default_repo_config = {}, $properties = {}, $local_repo = '' ) {
+define maven::settings( 
+	$target_directory = undef, $target_filename= 'settings.xml', 
+	$user = 'root',
+  	$servers = [], $mirrors = [], $default_repo_config = {}, $properties = {}, $local_repo = '' 
+) {
 
-  if $home == undef {
-    $home_real = $user ? {
-      'root'  => '/root',
-      default => "/home/${user}"
+  if $target_directory == undef {
+    $target_real = $user ? {
+      'root'  => '/root/.m2',
+      default => "/home/${user}/.m2"
     }
   }
   else {
-    $home_real = $home
+    $target_real = $target_directory
   }
 
-  file { "${home_real}/.m2":
-    ensure => directory,
-    owner  => $user,
-    mode   => '0700',
-  } ->
-  file { "${home_real}/.m2/settings.xml":
+#  file { "${target_real}":
+#    ensure => directory,
+#    owner  => $user,
+#    mode   => '0700',
+#  } ->
+## target directory creation should not be part of the definition
+## otherwise you can't use the definition several times
+  file { "${target_real}/${target_filename}":
     owner   => $user,
     mode    => '0600',
     content => template('maven/settings.xml.erb'),
+    require => File["${target_real}"],
   }
 
 }

4db48a8fd216c302356499840743513f15a94b92
SHaselbauer <sarah.haselbauer@akquinet.de>
2013-03-05 16:21:36 +0100

    added param ='LATEST'

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,7 @@ class maven::params {
 	$client_localrepo_dir ="$tmp_dir/client/repository"
 	$client_settings = "$client_tmp_dir/settings.xml" 
 	$client_settings_security = "$client_tmp_dir/settings-security.xml"
+	$client_latest_version_tag='LATEST'
 	
 	$cleanup_client = "mvn_tmp_client_cleanup"
 }

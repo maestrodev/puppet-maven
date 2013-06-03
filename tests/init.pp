8a471085919ae8290cce861cdeba3fe46417ffae
Carlos Sanchez <csanchez@maestrodev.com>
2013-06-02 22:44:17 -0700

    Add a Vagrantfile for testing

--- tests/init.pp
+++ tests/init.pp
@@ -1,3 +1,5 @@
+class { 'java': }
+
 $repo1 = {
   id       => 'myrepo',
   username => 'myuser',
@@ -5,18 +7,16 @@ $repo1 = {
   url      => 'http://repo.acme.com',
 }
 
-#class { 'maven::maven':
-#  version => '2.2.1',
-#} ->
+class { 'maven::maven': } ->
+
 #maven::settings { 'root' :
 #  servers => [$repo1],
 #}
 
 maven { '/tmp/maven-core-2.2.1.jar':
   id     => 'org.apache.maven:maven-core:2.2.1:jar',
-  #repos => ['file:///Users/csanchez/.m2/repository'],
   repos  => ['central::default::http://repo1.maven.apache.org/maven2','http://mirrors.ibiblio.org/pub/mirrors/maven2'],
-}
+} ->
 maven { '/tmp/maven-core-2.2.1-sources.jar':
   groupid    => 'org.apache.maven',
   artifactid => 'maven-core',

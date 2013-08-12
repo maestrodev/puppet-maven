c59f726e83dbcfaeb9dc2ff96fc1f937913b58ae
Carlos Sanchez <csanchez@maestrodev.com>
2013-08-12 13:43:25 +0200

    Print all warnings, not just the first spec ones

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
-Puppet::Util::Log.level = :warning
-Puppet::Util::Log.newdestination(:console)
+RSpec.configure do |c|
+
+  c.before(:each) do
+    Puppet::Util::Log.level = :warning
+    Puppet::Util::Log.newdestination(:console)
+  end
+
+end

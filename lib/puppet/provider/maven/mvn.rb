b87a9b6fddd17ec7fed21a3ee0ea0babb1d20608
Andreyev Dias de Melo <andreyev@cpqd.com.br>
2013-12-26 17:00:20 -0200

    Could not find puppet-maven profile, only the default-repo.

--- lib/puppet/provider/maven/mvn.rb
+++ lib/puppet/provider/maven/mvn.rb
@@ -104,7 +104,7 @@ Puppet::Type.type(:maven).provide(:mvn) do
 
     debug "mvn downloading (if needed) repo file #{msg} to #{dest} from #{repos.join(', ')}"
 
-    command = ["mvn org.apache.maven.plugins:maven-dependency-plugin:#{plugin_version}:get #{command_string} -DremoteRepositories=#{repos.join(',')} -Ddest=#{dest} -Dtransitive=false -Ppuppet-maven #{options}"]
+    command = ["mvn org.apache.maven.plugins:maven-dependency-plugin:#{plugin_version}:get #{command_string} -DremoteRepositories=#{repos.join(',')} -Ddest=#{dest} -Dtransitive=false -Pdefault-repo #{options}"]
 
     timeout = @resource[:timeout].nil? ? 0 : @resource[:timeout].to_i
     output = nil
